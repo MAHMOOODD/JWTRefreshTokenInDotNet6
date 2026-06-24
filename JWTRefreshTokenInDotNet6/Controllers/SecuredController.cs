d66dc7abf7064a4a0d1d8614f1c9b2effe24b890
Mahmoud Salah helaly <10812023101177@stud.cu.edu.eg>
2026-06-24 20:00:57 +0300

    Update SecuredController.cs

--- JWTRefreshTokenInDotNet6/Controllers/SecuredController.cs
+++ JWTRefreshTokenInDotNet6/Controllers/SecuredController.cs
@@ -13,6 +13,7 @@ namespace JWTRefreshTokenInDotNet6.Controllers
         public IActionResult GetData()
         {
             return Ok("Hello from secured controller");
+            
         }
     }
 }
